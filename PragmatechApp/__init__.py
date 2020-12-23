1b22673a41b15ed9655d65d6721b24f4d3ff0f25
Sevil2005 <sevillerasulova@gmail.com>
2020-12-23 20:33:21 +0400

    updated

--- PragmatechApp/__init__.py
+++ PragmatechApp/__init__.py
@@ -4,12 +4,14 @@ from flask_migrate import Migrate
 from PragmatechApp.config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
 
     from PragmatechApp.admin.routes import admin
     from PragmatechApp.user.routes import user
