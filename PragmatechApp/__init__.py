ae759bdb52dd5b103200c3b1ab5702d45ddf7396
Sevil2005 <sevillerasulova@gmail.com>
2020-12-23 22:30:58 +0400

    just I want to create db...

--- PragmatechApp/__init__.py
+++ PragmatechApp/__init__.py
@@ -1,22 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from PragmatechApp.config import Config
 
-db = SQLAlchemy()
-migrate = Migrate()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    
-    db.init_app(app)
-    migrate.init_app(app, db, render_as_batch=True)
+db = SQLAlchemy(app)
 
-    from PragmatechApp.admin.routes import admin
-    from PragmatechApp.user.routes import user
+from PragmatechApp.admin.routes import admin
+from PragmatechApp.user.routes import user
 
-    app.register_blueprint(admin)
-    app.register_blueprint(user)
-
-    return app
+app.register_blueprint(admin)
+app.register_blueprint(user)
