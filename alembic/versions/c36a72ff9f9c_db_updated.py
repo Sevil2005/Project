d996f06c0855f31996ba847a8a4faafc04ec44c2
Sevil2005 <sevillerasulova@gmail.com>
2020-12-24 13:30:54 +0400

    some changes made

---
+++ alembic/versions/c36a72ff9f9c_db_updated.py
@@ -0,0 +1,24 @@
+"""db updated
+
+Revision ID: c36a72ff9f9c
+Revises: 53ded7bd60d1
+Create Date: 2020-12-24 13:23:05.126446
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c36a72ff9f9c'
+down_revision = '53ded7bd60d1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
