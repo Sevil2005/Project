03c58bbff71e35c6b4ca8fc1aae67710f0b1e360
Sevil2005 <sevillerasulova@gmail.com>
2020-12-24 12:24:24 +0400

    db changed

---
+++ alembic/versions/8858a1b0c3b2_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 8858a1b0c3b2
+Revises: 
+Create Date: 2020-12-24 11:13:25.880166
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8858a1b0c3b2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from PragmatechApp import app
+from PragmatechApp import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
